38f3bee0259a6a73fdba57304eb03709ddf8ca72
Dipu Raj <hello@techlaboratory.net>
2017-05-17 12:04:22 +0530

    First release

---
+++ README.md
@@ -0,0 +1,41 @@
+# jQuery Gulp Plugin Example
+#### The example repository for jQuery plugin development with Gulp automation
+
+[![Build Status](https://travis-ci.org/techlab/jquery-gulp-example.svg?branch=master)](https://travis-ci.org/techlab/jquery-gulp-example)
+[![npm version](https://badge.fury.io/js/jquery-gulp-example.svg)](https://badge.fury.io/js/jquery-gulp-example)
+[![Bower version](https://badge.fury.io/bo/jquery-gulp-example.svg)](https://badge.fury.io/bo/jquery-gulp-example)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/techlab/jquery-gulp-example/master/LICENSE)
+[![Donate on Paypal](https://img.shields.io/badge/PayPal-dipuraj-blue.svg)](https://www.paypal.me/dipuraj)
+
+Articles
+-----
+  + [jQuery plugin development with Gulp automation](http://jsplanet.net/article/1003/jquery-plugin-development-gulp-automation)
+
+Requirements
+-----
+  + [jQuery](http://jquery.com/) (supports jQuery 1.9+, jQuery 2+ and jQuery 3+)
+
+Installation and usage 
+-----
+
+### [NPM](https://www.npmjs.com/package/jquery-gulp-example)
+    npm install jquery-gulp-example
+    
+### Bower
+    bower install jquery-gulp-example
+    
+### Download
+#### [Download from GitHub](https://github.com/techlab/jquery-gulp-example/archive/master.zip)    
+
+
+License
+----
+[MIT License](https://github.com/techlab/jquery-gulp-example/blob/master/LICENSE)
+
+Contribute
+----
+If you like the project please support with your contribution.
+
+[Donate on Paypal](https://www.paypal.me/dipuraj)
+
+Thank you :)
---
+++ test/test.js
@@ -0,0 +1,81 @@
+describe('Plugin Default Init', function() {
+    var el, plugin;
+
+    beforeEach(function(){
+        el = $('<div id="test-element"></div>');
+        $(document.body).append(el);
+        plugin = el.alterMe();
+    });
+    
+    afterEach(function(){
+        el.remove();
+        el = null;
+    });
+    
+    it('should add default theme to the element', function() {
+        expect(el).toHaveClass("theme-default");
+    });
+
+});
+
+describe('Plugin with Option Init', function() {
+    var el, plugin;
+
+    beforeEach(function(){
+        el = $('<div id="test-element"></div>');
+        $(document.body).append(el);
+        plugin = el.alterMe({ theme: 'blue'});
+    });
+    
+    afterEach(function(){
+        el.remove();
+        el = null;
+    });
+    
+    it('should add only blue theme to the element', function() {
+        expect(el.hasClass('theme-blue')).toBe(true);
+        expect(el).not.toHaveClass("theme-default");
+    });
+});
+
+describe('Convert to UPPERCASE', function() {
+    var el, plugin;
+    var text = "Hello this is aTest";
+
+    beforeEach(function(){
+        el = $('<div id="test-element">' + text + '</div>');
+        $(document.body).append(el);
+        plugin = el.alterMe({ alterType: 'upper'});
+    });
+    
+    afterEach(function(){
+        el.remove();
+        el = null;
+    });
+    
+    it('should convert the element text to UPPERCASE', function() {
+        expect(el.text()).toBe(text.toUpperCase());
+        expect(el.text()).not.toBe(text);
+    });
+});
+
+describe('Convert to lowercase', function() {
+    var el, plugin;
+    var text = "Hello this is aTest";
+
+    beforeEach(function(){
+        el = $('<div id="test-element">' + text + '</div>');
+        $(document.body).append(el);
+        plugin = el.alterMe({ alterType: 'lower'});
+    });
+    
+    afterEach(function(){
+        el.remove();
+        el = null;
+    });
+    
+    it('should convert the element text to lowercase', function() {
+        expect(el.text()).toBe(text.toLowerCase());
+        expect(el.text()).not.toBe(text);
+    });
+});
